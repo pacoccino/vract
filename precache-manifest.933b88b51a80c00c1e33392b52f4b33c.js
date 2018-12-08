3ee9ceda3677ae3ce97579a5e72fd2de4a30545a
Pacien Boisson <pakokrew@gmail.com>
2018-12-08 20:28:01 +0100

    Updates

---
+++ precache-manifest.933b88b51a80c00c1e33392b52f4b33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https//ngfar.io/vract/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "57d63990af591df8be4b",
+    "url": "https//ngfar.io/vract/static/js/runtime~main.57d63990.js"
+  },
+  {
+    "revision": "e84a0964a3ab402cdfc3",
+    "url": "https//ngfar.io/vract/static/js/main.e84a0964.chunk.js"
+  },
+  {
+    "revision": "35f54166abb96e4d574e",
+    "url": "https//ngfar.io/vract/static/js/1.35f54166.chunk.js"
+  },
+  {
+    "revision": "e84a0964a3ab402cdfc3",
+    "url": "https//ngfar.io/vract/static/css/main.44e81469.chunk.css"
+  },
+  {
+    "revision": "ef16ebed91d5ce84103928a65799d046",
+    "url": "https//ngfar.io/vract/index.html"
+  }
+];
