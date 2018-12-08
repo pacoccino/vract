d1c0eb770c7b7eaf30dc359f76a7d74703547177
Pacien Boisson <pakokrew@gmail.com>
2018-12-08 20:23:16 +0100

    Updates

---
+++ precache-manifest.5fb300130f6001d8147310d1b676daa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed39a6a47baa21cc79ef",
+    "url": "/static/js/main.ed39a6a4.chunk.js"
+  },
+  {
+    "revision": "35f54166abb96e4d574e",
+    "url": "/static/js/1.35f54166.chunk.js"
+  },
+  {
+    "revision": "ed39a6a47baa21cc79ef",
+    "url": "/static/css/main.44e81469.chunk.css"
+  },
+  {
+    "revision": "632e30ef8e502986115c6727dee18ea1",
+    "url": "/index.html"
+  }
+];
